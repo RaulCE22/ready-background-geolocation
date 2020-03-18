4708cea28e1ab54797a352d6d6b6b76caee3bee4
RaulCE22 <rce1991@gmail.com>
2020-03-18 12:10:39 +0000

    Access control

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ db.once('open', function() {
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//app.use(function(req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//  next();
-//});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('/', function(req,res){
   fs.readFile('./index.html', function (err, html) {
     if (err) throw err;    
