c8836d27182b091565c10c4034108d8ffdf39ffa
RaulCE22 <rce1991@gmail.com>
2020-03-18 13:24:27 +0000

    Final

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ db.once('open', function() {
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.get('/api/users',function(req,res){
   return userController.getAllUsers()
